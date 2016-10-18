86c053cedee5b27bdd70bcc6b0783da82550efdf
Kris Campos <kcampos0101@gmail.com>
2016-10-17 17:42:23 -0700

    Explained how the greet function worked

--- app/src/main/java/com/campos/kris/myapplication/MainActivity.java
+++ app/src/main/java/com/campos/kris/myapplication/MainActivity.java
@@ -14,6 +14,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Custom method that will greet the user. Can only be called
+     * by UI elements (know as View objects in Android)
+     *
+     * We are expecting the button to call this method. As such, we know
+     * that the View object that is passed in will be the button. However,
+     * we want the text that was written into the EditText View. So how
+     * do we get it?
+     *
+     * We can grab a reference to that object by asking the Android OS for it.
+     * findViewById is a method that will allow us to find any View by it's id.
+     * So we will pass in R.id.editText and it will return our EditText object
+     * as a View object.
+     *
+     * You can think of R as a phonebook that lets us quickly look up relevant objects.
+     *
+     * We cast that object to an EditText object and in the next line we can grab
+     * the text it is holding. We can now use that text to greet our user via a Toast.
+     *
+     * @param view - The UI element that called this method. The element
+     *             will pass itself in automatically when it calls this
+     *             method.
+     */
     public void greet(View view) {
         EditText nameField = (EditText)findViewById(R.id.editText);
         String message = nameField.getText().toString();
